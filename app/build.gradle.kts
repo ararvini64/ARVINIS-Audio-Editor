fd99c9ddbee34f60900f73ede918b784c07510c2
ararvini64 <ar.arvini64@gmail.com>
2026-07-24 19:52:33 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     
     // Compose UI
-    implementation("com.arthenica:ffmpeg-kit-full:6.0-2")
+  
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
