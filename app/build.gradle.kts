f090ca7b7b675498f4cf987470419c11f52d2d71
ararvini64 <ar.arvini64@gmail.com>
2026-07-24 11:06:56 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     
     // Compose UI
-    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
